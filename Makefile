ed6c721e4a35a7b610c5b7e59fde6dab5e41f453
Gabriel Florit <gabrielflorit@gmail.com>
2015-02-24 00:01:03 -0500

    more

--- Makefile
+++ Makefile
@@ -8,3 +8,8 @@ R_deploy:
 	cp data/02-23-2015-bondbill.html /Volumes/www_html/multimedia/graphics/projectFiles/Rmd/
 	rsync -rv data/02-23-2015-bondbill_files /Volumes/www_html/multimedia/graphics/projectFiles/Rmd
 	open http://private.boston.com/multimedia/graphics/projectFiles/Rmd/02-23-2015-bondbill.html
+
+prepare:
+
+	rm -rf data/data.json
+	csvcut data.csv -c Project,"Total Cost",Committed,FUNDED,Category | csvjson > data/data.json
--- graphics/chart/js/main.js
+++ graphics/chart/js/main.js
@@ -1,3 +1,46 @@
-// Put all your code here.
-// NOTE: no need to do $(document).ready, or $(function() { etc here.
-// Just start writing js.
+function log(s) {
+	console.log(JSON.stringify(s, null, 4));
+}
+
+var master = $('.article-graphic.chart');
+
+var data = _.chain(require('../../../data/data.json'))
+	.groupBy('FUNDED')
+	.map(function(projects, status) {
+		return {
+			status: status,
+			projects: _.chain(projects)
+				.map(function(project) {
+					project['Total Cost'] = +project['Total Cost'];
+					project.Committed = +project.Committed;
+
+					if (status !== 'unk') {
+						project.Delta = project['Total Cost'] - project.Committed;
+					}
+					return project;
+				})
+				.sortBy('Total Cost')
+				.reverse()
+				.value()
+		};
+	})
+	.sortBy(function(v, i) {
+		return _.indexOf(['partial', 'unfunded', 'fully', 'unk'], v.status);
+	})
+	.value();
+
+var max = _.chain(data)
+	.reject({status: 'unk'})
+	.pluck('projects')
+	.flatten()
+	.pluck('Total Cost')
+	.max()
+	.value();
+
+var html = data.map(function(v, i) {
+	var html = _.templates.projects(_.extend(v, {max: max}));
+
+	return html;
+}).join('');
+
+$('.content', master).html(html);
